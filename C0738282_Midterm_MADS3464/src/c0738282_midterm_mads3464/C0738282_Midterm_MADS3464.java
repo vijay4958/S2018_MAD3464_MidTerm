0c97c14b8382e22df0dbbffbfd11b5e4d5a55f85
MacStudent <macstudent@20X-iMacXX-M-386.local>
2018-06-07 17:10:28 -0400

    commited

--- C0738282_Midterm_MADS3464/src/c0738282_midterm_mads3464/C0738282_Midterm_MADS3464.java
+++ C0738282_Midterm_MADS3464/src/c0738282_midterm_mads3464/C0738282_Midterm_MADS3464.java
@@ -1,3 +1,6 @@
+
+import c0738282_midterm_mads3464.LambtonStringTools;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,9 +16,13 @@ public class C0738282_Midterm_MADS3464 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
        
-        LambtonStringTools lma = new LambtonStringTools
+     LambtonStringTools lma = new LambtonStringTools();
+    System.out.println("First Method Execution - Reverse a String");
+       lma.reverse("lambton");
+        System.out.println(" \n Second Method Execution - StringToBinary");
+    lma.binaryToDecimal("1000");
     }
     
 }
--- C0738282_Midterm_MADS3464/src/c0738282_midterm_mads3464/LambtonStringTools.java
+++ C0738282_Midterm_MADS3464/src/c0738282_midterm_mads3464/LambtonStringTools.java
@@ -12,11 +12,11 @@ public class LambtonStringTools {
     
     static int a;
     static int bin;
-    static int binary[];
-    static int index;
+    static int deci = 0;
+    static int pwr = 0
     
     
-    static String reverse(String s){
+    public String reverse(String s){
         char ch[] = new char[s.length()];
         for(a = 0;a < s.length();a++){
             ch[a] = s.charAt(a);
@@ -29,8 +29,23 @@ public class LambtonStringTools {
     
     
     
-      public int binaryToDecimal(String binary){
-   numbers = binary.toCharArray();
-   
-    }
-}
+      public int binaryToDecimal(String s){
+          bin = Integer.parseInt(s);
+          while(true){
+             if(bin == 0)
+             {
+                 break;
+                 {
+             else
+                     {
+                             int demi = bin^10;
+                             deci+=demi(*Math.pow(2,pwr);
+                             bin = bin/10;
+                                     pwr++;
+                             }
+                     
+         
+      }
+    return deci;     
+             }
+             public String 
