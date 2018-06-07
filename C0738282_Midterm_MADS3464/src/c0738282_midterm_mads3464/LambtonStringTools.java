7ded3eac2c89df1c22527f9e5a0434320809e2cb
MacStudent <macstudent@20X-iMacXX-M-386.local>
2018-06-07 15:56:54 -0400

    Pritesh commited this code

---
+++ C0738282_Midterm_MADS3464/src/c0738282_midterm_mads3464/LambtonStringTools.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0738282_midterm_mads3464;
+/**
+ *
+ * @author macstudent
+ */
+public class LambtonStringTools {
+    
+    static int a;
+    static int bin;
+    static int res;
+    
+    static String reverse(String s){
+        char ch[] = new char[s.length()];
+        for(a = 0;a < s.length();a++){
+            ch[a] = s.charAt(a);
+               }
+          for(a=s.length()-1; a>=0;a++)  {   
+              System.out.print(ch[a]);
+             }   
+          return s;
+        }
+    
+    
+    
+      public int binaryToDecimal(String s){
+          s.toCharArray() = new
+    }
+}
